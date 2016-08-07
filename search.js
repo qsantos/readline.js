3961f46841ab2fe1560b7c569ae11a3e2e694106
Quentin Santos <quentin.santos@worldonline.fr>
2016-08-07 14:50:36 +0200

    Implement rl_{forward,reverse}_search_history

--- search.js
+++ search.js
@@ -2,6 +2,91 @@
 
 var _rl_noninc_text;
 
+/* Search through the history looking for an interactively typed string.
+   This is analogous to i-search.  We start the search in the current line.
+   DIRECTION is which direction to search; >= 0 means forward, < 0 means
+   backwards. */
+function rl_search_history(direction) {
+    rl_redisplay();  // avoid a redisplay from clearing out the prompt
+
+    var pattern = '';
+    var match = '';
+    function redisplay() {
+        // first set the line buffer to the first history match
+        var index = rl_history_index;
+        while (true) {
+            // next history entry
+            if (direction < 0) {
+                index--;
+                if (index < 0) {
+                    break;
+                }
+            } else {
+                index++;
+                if (index >= rl_history.length) {
+                    break;
+                }
+            }
+            var line = rl_history[index];
+
+            // search for a match
+            var offset = line.indexOf(pattern);
+            if (offset < 0) {
+                continue;
+            }
+            match = line;
+
+            // found one! let us alter the line buffer
+            var decorated = '';
+            decorated += line.substring(0, offset);
+            decorated += '\x1b[4m';  // underline
+            decorated += line.substring(offset, offset + pattern.length);
+            decorated += '\x1b[m';  // reset attributes
+            decorated += line.substring(offset + pattern.length);
+            rl_line_buffer = decorated;
+            rl_point = decorated.length;
+            rl_redisplay();
+            break;
+        }
+
+        // now we show the prompt
+
+        // temporarily go to next line
+        write('\x1b[s');  // save cursor state
+        write('\n');  // next line
+        write('\x1b[2K');  // clear line
+
+        if (direction < 0) {
+            write('bck');
+        } else {
+            write('fwd');
+        }
+        write('-i-search: ' + pattern + '_');
+        write('\x1b[u');  // restore cursor state
+    }
+    redisplay();
+
+    function type(key) {
+        if (key.length == 1 && key >= ' ') {
+            pattern += key;
+            redisplay();
+            rl_read_key(type);
+        } else if (key == 'Backspace') {
+            pattern = pattern.substring(0, pattern.length-1);
+            redisplay();
+            rl_read_key(type);
+        } else {
+            write('\x1b[s');  // save cursor state
+            write('\n');  // next line
+            write('\x1b[2K');  // clear line
+            write('\x1b[u');  // restore cursor state
+            rl_line_buffer = match;
+            rl_point = match.length;
+        }
+    }
+    rl_read_key(type);
+}
+
 /* Search non-interactively through the history list.  DIR < 0 means to
    search backwards through the history of previous commands; otherwise
    the search is for commands subsequent to the current position in the
@@ -39,8 +124,17 @@ function rl_noninc_search(dir) {
 /* Bindable commands for incremental searching. */
 /************************************************/
 
-//extern int rl_reverse_search_history PARAMS((int, int));
-//extern int rl_forward_search_history PARAMS((int, int));
+/* Search backwards through the history looking for a string which is typed
+   interactively.  Start with the current line. */
+function rl_reverse_search_history(count, key) {
+    rl_search_history(-count, key);
+}
+
+/* Search forwards through the history looking for a string which is typed
+   interactively.  Start with the current line. */
+function rl_forward_search_history(count, key) {
+    rl_search_history(count, key);
+}
 
 
 /****************************************************************************/
